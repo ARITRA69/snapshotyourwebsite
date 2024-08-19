80898d2be2268803301fa365a059089662b121c0
Aritra Sarkar <aritrasarkar2002@gmail.com>
2024-08-19 21:57:10 +0530

    server up and running

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
